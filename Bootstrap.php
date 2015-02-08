ba28c5202b38585ab59bfbea04bf0e87f7ae9fd2
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-08 13:33:24 +0000

    added bootstrap script
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

---
+++ Bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the mata project.
+ *
+ * (c) mata project <http://github.com/mata/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace mata\user;
+
+use yii\authclient\Collection;
+use yii\base\BootstrapInterface;
+use yii\base\InvalidConfigException;
+use yii\i18n\PhpMessageSource;
+use yii\web\GroupUrlRule;
+use yii\console\Application as ConsoleApplication;
+use yii\web\User;
+
+/**
+ * Bootstrap class registers module and user application component. It also creates some url rules which will be applied
+ * when UrlManager.enablePrettyUrl is enabled.
+ *
+ * @author Dmitry Erofeev <dmeroff@gmail.com>
+ */
+class Bootstrap implements BootstrapInterface {
+    /** @inheritdoc */
+    public function bootstrap($app) {
+        echo "Bootstrapping Menus";
+    }
+}
+
