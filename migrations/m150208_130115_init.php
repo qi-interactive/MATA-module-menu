5ed9f6c01fb933c80b884cff6a540edcc133225f
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-08 13:22:01 +0000

    added migrations
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

---
+++ migrations/m150208_130115_init.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the mata project.
+ *
+ * (c) mata project <http://github.com/mata/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use yii\db\Schema;
+use mata\user\migrations\Migration;
+
+/**
+ * @author Dmitry Erofeev <dmeroff@gmail.com
+ */
+class m150208_130115_init extends Migration
+{
+	public function up()
+	{
+		$this->createTable('{{%matamodulemenu_group}}', [
+			'Id'                   => Schema::TYPE_PK,
+			'Name'             => Schema::TYPE_STRING . '(128) NOT NULL',
+			'Order'                => Schema::TYPE_SMALLINT . '(2) NOT NULL',
+			], $this->tableOptions);
+
+		$this->createTable('{{%matamodulemenu_module}}', [
+			'Id'        => Schema::TYPE_INTEGER . ' PRIMARY KEY',
+			'Name'           => Schema::TYPE_STRING . '(64)',
+			'GroupId'   => Schema::TYPE_INTEGER . '(11) NOT NULL',
+			'Config' => Schema::TYPE_TEXT,
+			], $this->tableOptions);
+
+		$this->addForeignKey('fk_matamodulemenugroup_matamodulemenumodule', '{{%matamodulemenu_module}}', 'GroupId', '{{%matamodulemenu_group}}', 'Id', 'CASCADE', 'RESTRICT');
+	}
+
+	public function down()
+	{
+		$this->dropTable('{{%matamodulemenu_module}}');
+		$this->dropTable('{{%matamodulemenu_group}}');
+
+	}
+}
