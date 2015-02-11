9bc95a74b81175f2c082c29c6cbf041f6051fa1f
Marcin Wiatr <marcin@qi-interactive.com>
2015-02-11 22:19:57 +0000

    updated location for tests
    Signed-off-by: Marcin Wiatr <marcin@qi-interactive.com>

--- tests/codeception/_bootstrap.php
+++ tests/codeception/_bootstrap.php
@@ -6,10 +6,10 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 defined('YII_TEST_ENTRY_URL') or define('YII_TEST_ENTRY_URL', '/index.php');
 defined('YII_TEST_ENTRY_FILE') or define('YII_TEST_ENTRY_FILE', __DIR__ . '/../application/web/index.php');
 
-defined('VENDOR_DIR') or define('VENDOR_DIR', __DIR__ . '/../../../../../vendor');
+defined('VENDOR_DIR') or define('VENDOR_DIR', __DIR__ . '/../../../..');
 
-require_once(VENDOR_DIR . '/autoload.php');
-require_once(VENDOR_DIR . '/yiisoft/yii2/Yii.php');
+// require_once(VENDOR_DIR . '/autoload.php');
+ 	require_once(VENDOR_DIR . '/yiisoft/yii2/Yii.php');
 
 $_SERVER['SCRIPT_FILENAME'] = YII_TEST_ENTRY_FILE;
 $_SERVER['SCRIPT_NAME']     = YII_TEST_ENTRY_URL;
