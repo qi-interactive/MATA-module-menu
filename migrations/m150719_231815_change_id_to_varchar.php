913c6d383845ffb364d145ac42758f49b7532f94
Michal Fiedorowicz <michal@qi-interactive.com>
2015-07-20 09:22:08 +0100

    Added migration for matamodulemenu_module (change Id from Int to Varchar)

---
+++ migrations/m150719_231815_change_id_to_varchar.php
@@ -0,0 +1,27 @@
+<?php
+ 
+/**
+ * @link http://www.matacms.com/
+ * @copyright Copyright (c) 2015 Qi Interactive Limited
+ * @license http://www.matacms.com/license/
+ */
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150719_231815_change_id_to_varchar extends Migration
+{
+	public function safeUp()
+	{
+		$this->alterColumn('{{%matamodulemenu_module}}', 'Id', Schema::TYPE_INTEGER . ' NOT NULL');
+		$this->dropPrimaryKey('PRIMARY', '{{%matamodulemenu_module}}');
+		$this->alterColumn('{{%matamodulemenu_module}}', 'Id', Schema::TYPE_STRING . '(64) NOT NULL PRIMARY KEY');
+	}
+
+	public function safeDown()
+	{
+		$this->alterColumn('{{%matamodulemenu_module}}', 'Id', Schema::TYPE_INTEGER . ' NOT NULL');
+		$this->dropPrimaryKey('PRIMARY', '{{%matamodulemenu_module}}');
+		$this->alterColumn('{{%matamodulemenu_module}}', 'Id', Schema::TYPE_PK);
+	}
+}
